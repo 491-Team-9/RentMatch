0b0cd3e3031418e3c0a294277227cceb0408a2f8
Keith Okuna <kokuna@me.com>
2021-10-16 12:49:53 -1000

    basic rental card

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Grid, Image } from 'semantic-ui-react';
+import RentalCard from '../components/RentalCard';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
@@ -7,13 +8,8 @@ class Landing extends React.Component {
     return (
       <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
 
-        <Grid.Column width={4}>
-          <Image size='small' circular src="/images/meteor-logo.png"/>
-        </Grid.Column>
-
         <Grid.Column width={8}>
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+          <RentalCard/>
         </Grid.Column>
 
       </Grid>
