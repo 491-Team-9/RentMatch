e823785d502c126aef360850d62fc4b7e93aece3
Keith Okuna <kokuna@me.com>
2021-10-16 13:19:16 -1000

    card improvements

--- app/imports/ui/components/RentalCard.jsx
+++ app/imports/ui/components/RentalCard.jsx
@@ -7,8 +7,40 @@ import './rental-card.css';
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 class RentalCard extends React.Component {
   render() {
+    let data = this.props.rental;
     return (
-        <div className="rental-card">hello</div>
+    <div className="ui card">
+        <div className="content">
+            <div className="header">
+                {data.name}
+            </div>
+            <div className="meta">{data.bedrooms}br/{data.bathrooms}ba</div>
+            <div className="meta">{data.location}</div>
+        </div>
+        <div className="image">
+            <img src="https://about.hawaiilife.com/wp-content/uploads/2018/06/View-from-Penthouse-at-the-Hokua-at-1288-Ala-Moana-e1528127882669.jpg"/>
+        </div>
+        <div className="content left aligned">
+            <span className="right floated">
+            <i className="heart outline like icon"></i>
+            17 likes
+            </span>
+            <i className="comment icon"></i>
+            3 questions
+        </div>
+        <div className="content left aligned">
+            {data.description}
+        </div>
+        <div className="extra content">
+            <div className="ui large transparent left floated icon button">
+                <i className="close red icon"></i>
+            </div>
+            <div className="ui large transparent right floated icon button">
+                <i className="heart blue icon"></i>
+            </div>
+        </div>
+    </div>
+
     );
   }
 }
@@ -19,10 +51,11 @@ RentalCard.propTypes = {
     _id: PropTypes.string,
     name: PropTypes.string,
     description: PropTypes.string,
+    location: PropTypes.string,
     price: PropTypes.number,
     rooms: PropTypes.number,
     bathrooms: PropTypes.number,
-    bathrooms: PropTypes.number,
+    images: PropTypes.array,
   }).isRequired,
 };
 
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,13 +5,15 @@ import RentalCard from '../components/RentalCard';
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
+    let rental = {
+      name: "Amazing apartment in downtown honolulu",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    }
     return (
-      <Grid id='landing-page' verticalAlign='middle' textAlign='center' container>
-
-        <Grid.Column width={8}>
-          <RentalCard/>
+      <Grid centered id='landing-page' verticalAlign='middle' container>
+        <Grid.Column>
+          <RentalCard rental={rental}/>
         </Grid.Column>
-
       </Grid>
     );
   }
