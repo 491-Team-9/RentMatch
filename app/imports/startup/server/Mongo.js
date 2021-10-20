f447c502cc44b636eea9f2baa4f359c363128b42
Joshua Hartmann <45891115+joshuahartmann@users.noreply.github.com>
2021-10-20 06:36:39 -1000

    default rental data

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Stuffs } from '../../api/stuff/Stuff.js';
-// import { Rentals } from '../../api/rental/Rental';
+import { Rentals } from '../../api/rental/Rental';
 
 /* eslint-disable no-console */
 
@@ -15,5 +15,19 @@ if (Stuffs.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
     console.log('Creating default data.');
     Meteor.settings.defaultData.map(data => addData(data));
+  }
+}
+
+// Initialize the database with a default data document.
+function addRentalData(data) {
+  console.log(`  Adding: ${data.title}`);
+  Rentals.collection.insert(data);
+}
+
+// Initialize the RentalsCollection if empty.
+if (Rentals.collection.find().count() === 0) {
+  if (Meteor.settings.defaultRentals) {
+    console.log('Creating default rental data.');
+    Meteor.settings.defaultRentals.map(data => addRentalData(data));
   }
 }
