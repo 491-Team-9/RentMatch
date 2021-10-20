c5bed6e7275b92dccc73d0053eaa7c4f1bfbb493
Keith Okuna <kokuna@me.com>
2021-10-19 23:50:43 -1000

    fixed user profile stuff

--- app/imports/ui/components/UserProfileCard.jsx
+++ app/imports/ui/components/UserProfileCard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Icon, Button } from 'semantic-ui-react';
+import { Card, Icon, Button, Modal } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
@@ -14,10 +14,10 @@ class UserProfileCard extends React.Component {
     this.acceptAction = this.acceptAction.bind(this);
   }
 
-    acceptAction() {
-        Meteor.call('rentals.approveLiker', {rentalId: this.props.rental._id, likerId: this.props.userProfileId });
-        Meteor.call('users.connectUsers', { userId1: this.props.userProfileId, userId2: Meteor.user()._id });
-    }
+  acceptAction() {
+    Meteor.call('rentals.approveLiker', { rentalId: this.props.rental._id, likerId: this.props.userProfileId });
+    Meteor.call('users.connectUsers', { userId1: this.props.userProfileId, userId2: Meteor.user()._id });
+  }
 
   rejectAction() {
 
@@ -26,13 +26,32 @@ class UserProfileCard extends React.Component {
   render() {
     // console.log(this.props);
     const user = this.props.users.find(u => u._id === this.props.userProfileId);
+    console.log(user);
     if (!user) {
       return ('loading...');
+    }
+    if (!user.profile) {
+      Meteor.call('user.addProfileInfo', user._id);
     }
     return (
       <Card>
         <Card.Content>
-          <Card.Header>{user.emails[0].address}</Card.Header>
+          <Modal trigger={
+            <Card.Header>
+              {user.profile?.email}
+            </Card.Header>
+          }>
+            <Modal.Content>
+              <Card>
+                <Card.Content>
+                  <Card.Header>{user.profile?.firstName} {user.profile?.lastName}</Card.Header>
+                  <Card.Meta>{user.profile?.email}</Card.Meta>
+                  <Card.Meta><Icon name="users" /> {user.profile?.renters} <Icon name="paw" /> {user.profile?.pets}</Card.Meta>
+                  <Card.Description>{user.profile?.biography}</Card.Description>
+                </Card.Content>
+              </Card>
+            </Modal.Content>
+          </Modal>
         </Card.Content>
         {/* middle section of the card */}
         <Card.Content extra>
